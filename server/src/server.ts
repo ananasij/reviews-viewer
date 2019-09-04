82a11d369357d0cf5a1caffa75fc2afdfca65275
Hanna Senkevich <hanna.senkevich@gmail.com>
2019-09-04 13:04:03 +0200

    Read data from file

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,8 @@ import * as Koa from 'koa';
 import * as Router from '@koa/router';
 import * as cors from '@koa/cors';
 import * as logger from 'koa-logger';
+import * as fs from 'fs';
+import * as path from 'path';
 
 import { routes } from './routes';
 import { Route } from './types';
@@ -9,34 +11,43 @@ import { Route } from './types';
 const app = new Koa();
 const router = new Router();
 
+// Read data from JSON file
+const reviewsDataFile = fs.readFileSync(
+  path.join(__dirname, 'data/reviews.json'),
+  'utf8'
+);
+export const reviewsData = JSON.parse(reviewsDataFile);
+
 app.use(logger());
 app.use(cors());
 
 app.use(async (ctx, next) => {
-    try {
-        await next();
-    } catch (err) {
-        ctx.status = err.status || 500;
-        ctx.body = err.message;
-        ctx.app.emit('error', err, ctx);
-    }
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+    ctx.app.emit('error', err, ctx);
+  }
 });
 
 app.on('error', (err, ctx) => {
-    // TODO: centralized error handling;
-    console.error(err);
+  // TODO: centralized error handling;
+  console.error(err);
 });
 
 router.get('/', (ctx, next) => {
-    ctx.body = 'Hello world!';
+  ctx.body = 'Hello world!';
 });
 
-routes.map((route: Route) => router[route.method](route.path, ...route.actions));
+routes.map((route: Route) =>
+  router[route.method](route.path, ...route.actions)
+);
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`Server is listening to port ${port}.`)
+  console.log(`Server is listening to port ${port}.`);
 });
